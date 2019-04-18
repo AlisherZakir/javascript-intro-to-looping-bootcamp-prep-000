6659c352fa3291825a4ab32e713c99073ae16904
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 19:08:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(arr) {
   for(var i = 0; i < 25; i++) {
-    arr.push(`I am ${i} string loop${i === 0 ? '' : 's'}`)
+    arr.push(`I am ${i} string loop${i === 0 ? '' : 's'}.`)
   }
   return arr;
 }
